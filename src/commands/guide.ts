cab32689cc1ba403901954297da2d1c58126759b
valshia <96647045+valshia@users.noreply.github.com>
2024-12-24 23:26:52 +0900

    update guide command (paging)

--- src/commands/guide.ts
+++ src/commands/guide.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction } from 'discord.js';
-import { Guide, guide_init, guide_titles, guide_contents, infoEmbed, errorEmbed } from '../messages';
+import { Guide, guideInit, guideTitles, guideContents, infoEmbed, errorEmbed, guideEmbed } from '../messages';
 
 const { MessageActionRow, MessageButton } = require('discord.js');
 const client = require('../index');
@@ -29,6 +29,12 @@ const row_jp = new MessageActionRow()
       .setCustomId('jpOtherInfo')
       .setLabel('3️⃣ その他の情報')
       .setStyle('PRIMARY'),
+  )
+  .addComponents(
+    new MessageButton()
+      .setCustomId('jpQandA')
+      .setLabel('4️⃣ Q&A')
+      .setStyle('PRIMARY'),
   );
 
 const row_en = new MessageActionRow()
@@ -55,6 +61,12 @@ const row_en = new MessageActionRow()
       .setCustomId('enOtherInfo')
       .setLabel('3️⃣ Other information')
       .setStyle('PRIMARY'),
+  )
+  .addComponents(
+    new MessageButton()
+      .setCustomId('enQandA')
+      .setLabel('4️⃣ Q&A')
+      .setStyle('PRIMARY'),
   );
 
 module.exports = {
@@ -75,30 +87,119 @@ module.exports = {
 
   async execute(interaction: CommandInteraction) {
     const lang = interaction.options.get('language');
+    console.log('Interaction received:', interaction);
     if (lang === null) {
       interaction.reply({
-        embeds: [errorEmbed('`url` slash command parameter is missing. This should not happen if the bot is properly configured.')]
+        embeds: [errorEmbed('Slash command parameter is missing. This should not happen if the bot is properly configured.')]
       });
       return;
     }
     if (lang.value === 'en') {
-      await interaction.reply({embeds: [infoEmbed(guide_init.en_title, guide_init.en_content)], components: [row_en]});
+      await interaction.reply({embeds: [infoEmbed(guideInit.en_title, guideInit.en_content)], components: [row_en]});
     } else if (lang.value === 'jp') {
-      await interaction.reply({embeds: [infoEmbed(guide_init.jp_title, guide_init.jp_content)], components: [row_jp]});
+      await interaction.reply({embeds: [infoEmbed(guideInit.jp_title, guideInit.jp_content)], components: [row_jp]});
     }
   },
 };
 
+function chunkText(text: string, pageBreakCode: string = "<FF>"): string[] {
+  return text.split(pageBreakCode);
+}
+
+function createPaginationButtons(page: number, totalPages: number){
+  const row = new MessageActionRow().addComponents(
+    new MessageButton()
+      .setCustomId('prevPage')
+      .setLabel('⬅️Previous/前のページ')
+      .setStyle('PRIMARY')
+      .setDisabled(page === 0),
+    new MessageButton()
+      .setCustomId('nextPage')
+      .setLabel('Next/次のページ➡️')
+      .setStyle('PRIMARY')
+      .setDisabled(page === totalPages-1),
+    new MessageButton()
+      .setCustomId('close_ephemeral')
+      .setLabel('Close/閉じる')
+      .setStyle('DANGER')
+  );
+  return row;
+}
+
+// ボタンのインタラクションを管理するためのマップ
+const activeInteractions = new Map<string, { 
+  page: number; 
+  contentChunks: string[]; 
+  title: string; 
+  timestamp: number; // データの追加時刻を追跡
+}>();
+
+// 定期的に古いデータを削除する
+setInterval(() => {
+  const now = Date.now();
+  activeInteractions.forEach((value, key) => {
+    if (now - value.timestamp > 3600000) { // 1時間（3600000ミリ秒）以上経過
+      activeInteractions.delete(key);
+    }
+  });
+}, 60000); // 毎分チェック
+
+
 client.on('interactionCreate', async (interaction: CommandInteraction) => {
-  if (!interaction.isButton()) {
-    return;
-  }
-  const emb_title = guide_titles[interaction.customId as keyof Guide];
-  const emb_content = guide_contents[interaction.customId as keyof Guide];
-  if (emb_title != null && emb_content != null) {
-    await interaction.reply({
-      embeds: [infoEmbed(emb_title, emb_content)],
+  if (!interaction.isButton()) return;
+  const now = new Date();
+  console.log(`${now}: ${interaction.user.username} just pressed ${interaction.customId} button.`);
+
+  if (interaction.customId === 'close_ephemeral') {
+    await interaction.deferUpdate();
+    await interaction.deleteReply();
+
+  } else if (interaction.customId == 'prevPage' || interaction.customId == 'nextPage'){
+    const customId = interaction.customId;
+    const messageId = interaction.message.id;
+    const data = activeInteractions.get(messageId);
+
+    if (!data) {
+        await interaction.reply({
+          content: 'This interaction has expired. Please close it and press the button again. 最初の画面からもう一度お試しください。',
+          ephemeral: true
+        });
+        return;
+    }
+
+    const { page, contentChunks, title } = data;
+
+    if (customId === 'nextPage' && page < contentChunks.length - 1) {
+        data.page++;
+    } else if (customId === 'prevPage' && page > 0) {
+        data.page--;
+    }
+
+    const newEmbed = guideEmbed(title, contentChunks, data.page);
+    const newButtons = createPaginationButtons(data.page, contentChunks.length);
+
+    await interaction.update({ embeds: [newEmbed], components: [newButtons] });
+
+  } else {
+    const title = guideTitles[interaction.customId as keyof Guide];
+    const content = guideContents[interaction.customId as keyof Guide];
+    const contentChunks = chunkText(content);
+
+    const initialEmbed = guideEmbed(title, contentChunks, 0);
+    const buttons = createPaginationButtons(0, contentChunks.length);
+
+    const message = await interaction.reply({
+      embeds: [initialEmbed],
+      components: [buttons],
+      fetchReply: true,
       ephemeral: true
+    });
+
+    activeInteractions.set(message.id, { 
+        page: 0, 
+        contentChunks, 
+        title, 
+        timestamp: Date.now() 
     });
   }
 });
--- src/messages.ts
+++ src/messages.ts
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
-const { msgJpToDoFirst, msgJpHowToEnjoy, msgJpChannelList, msgJpOtherInfo,
-        msgEnToDoFirst, msgEnHowToEnjoy, msgEnChannelList, msgEnOtherInfo } = require('./guide_messages');
+const { msgJpToDoFirst, msgJpHowToEnjoy, msgJpChannelList, msgJpOtherInfo, msgJpQandA,
+        msgEnToDoFirst, msgEnHowToEnjoy, msgEnChannelList, msgEnOtherInfo, msgEnQandA } = require('./guide_messages');
 
 export const messages = {
   hello: 'へい！こんかなた〜！',
@@ -12,39 +12,45 @@ export interface Guide {
   jpHowToEnjoy:  string;
   jpChannelList: string;
   jpOtherInfo:   string;
+  jpQandA:       string;
   enToDoFirst:   string;
   enHowToEnjoy:  string;
   enChannelList: string;
   enOtherInfo:   string;
-}
+  enQandA:       string;
+};
 
-export const guide_init = {
+export const guideInit = {
   jp_title: 'サーバのご案内',
   jp_content: '私達のサーバへようこそ！知りたい情報のボタンを押してください。',
   en_title: 'Server Information',
   en_content: 'Welcome to our server! Please press the button for the information you want to know.',
 };
 
-export const guide_titles = {
+export const guideTitles = {
   jpToDoFirst: '0️⃣ 初めにやること',
   jpHowToEnjoy: '1️⃣ サーバの楽しみ方',
   jpChannelList: '2️⃣ チャンネル一覧',
   jpOtherInfo: '3️⃣ その他の情報',
+  jpQandA: '4️⃣ Q&A',
   enToDoFirst: '0️⃣ To do first',
   enHowToEnjoy: '1️⃣ How to enjoy our server',
   enChannelList: '2️⃣ List of channels',
-  enOtherInfo: '3️⃣ Other information'
+  enOtherInfo: '3️⃣ Other information',
+  enQandA: '4️⃣ Q&A'
 } as Guide;
 
-export const guide_contents = {
+export const guideContents = {
   jpToDoFirst: msgJpToDoFirst,
   jpHowToEnjoy: msgJpHowToEnjoy,
   jpChannelList: msgJpChannelList,
   jpOtherInfo: msgJpOtherInfo,
+  jpQandA: msgJpQandA,
   enToDoFirst: msgEnToDoFirst,
   enHowToEnjoy: msgEnHowToEnjoy,
   enChannelList: msgEnChannelList,
-  enOtherInfo: msgEnOtherInfo
+  enOtherInfo: msgEnOtherInfo,
+  enQandA: msgEnQandA
 } as Guide;
 
 export function errorEmbed(message: string) {
@@ -53,7 +59,7 @@ export function errorEmbed(message: string) {
     .setTitle('Error!')
     .setDescription(message);
   return emb;
-}
+};
 
 export function infoEmbed(title: string, message: string) {
   const emb = new MessageEmbed()
@@ -61,4 +67,15 @@ export function infoEmbed(title: string, message: string) {
     .setTitle(title)
     .setDescription(message);
   return emb;
-}
+};
+
+export function guideEmbed(title: string, contentChunks: string[], page: number){
+  const emb = new MessageEmbed()
+    .setTitle(title)
+    .setDescription(contentChunks[page])
+    .setFooter({
+      text: `(Page ${page + 1}/${contentChunks.length})`,
+    })
+    .setColor('#00AAFF');
+    return emb
+};
